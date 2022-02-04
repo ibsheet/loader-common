1ce4b8bcf8e3eb9a34482f5313ee68728637b106
이재호 <jhlee@ibleaders.co.kr>
2022-02-04 09:49:35 +0900

    hasOwnProperty eslint

--- src/convert_v7/7version.js
+++ src/convert_v7/7version.js
@@ -5,7 +5,7 @@ function clone(obj) {
   if (obj === null || typeof (obj) !== 'object') return obj;
   var copy = obj.constructor();
   for (var attr in obj) {
-    if (obj.hasOwnProperty(attr)) {
+    if (obj.prototype.hasOwnProperty.call(obj, attr)) {
       copy[attr] = clone(obj[attr]);
     }
   }
